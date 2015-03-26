3317a53a7b8479ea5131d220710e484e23591cc7
SutanKasturi <sutankasturi@yahoo.com.my>
2015-03-27 00:19:51 +0800

    Second commit

--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,5 +1,6 @@
 module ParentsHelper
   def select_parent_family_tag(form, object, options={})
-  	form.select :family_id, options_for_select(@families.map{|family| [family.name, family.id]}, selected: object.family_id), options.merge(include_blank: '-- Select Family --')
+  	form.select :family_id, options_for_select(@families.map{|family| [family.name, family.id]}, selected: object.family_id), 
+  	options.merge(include_blank: '-- Select Family --')
   end
 end
